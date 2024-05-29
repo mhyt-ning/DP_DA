6bd024b35bc1669985b1d52b735e0b5da9e54714
Juhua Zhang <1508598093@qq.com>
2024-05-29 09:53:48 +0800

    Update Teacher.py

--- Teacher.py
+++ Teacher.py
@@ -1,46 +1,53 @@
-import torch,gc
+import torch, gc
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.distributions.laplace import Laplace
 from torch.distributions import Normal
+from util import accuracy
 # import syft.frameworks.torch.dp.pate as pate
 from my_framework import pate
 
-from transformers import RobertaForSequenceClassification, AdamW
+from transformers import AutoTokenizer, RobertaForSequenceClassification, AdamW
+from torch.utils.data import DataLoader, TensorDataset, Dataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
 from tqdm import tqdm
 import os
+from torch.nn.functional import softmax
+
 
 class Teacher:
     """Implementation of teacher models.
        Teacher models are ensemble of models which learns directly disjoint splits of the sensitive data
-       The ensemble of teachers are further used to label unlabelled public data on which the student is 
-       trained. 
+       The ensemble of teachers are further used to label unlabelled public data on which the student is
+       trained.
        Args:
            args[Arguments object]: An object of Arguments class with required hyperparameters
            n_teachers[int]: Number of teachers
            epochs[int]: Number of epochs to train each model
     """
 
-    def __init__(self, args, n_teachers=1, is_init=1,sigma=1,epsilon=0.5):
+    def __init__(self, args, n_teachers=1, is_init=1, sigma=1, epsilon=0.5):
 
         self.n_teachers = n_teachers
         self.models = {}
         self.args = args
-        if is_init==1:
+        if is_init == 1:
             self.init_models()
         self.epsilon = 100
-        self.sigma=sigma
+        self.sigma = sigma
 
     def init_models(self):
         """Initialize teacher models according to number of required teachers"""
 
         name = "model_"
         for index in range(0, self.n_teachers):
-
-            model = RobertaForSequenceClassification.from_pretrained('../model/roberta-large',
-                                                             num_labels=2)
+            model = RobertaForSequenceClassification.from_pretrained(
+                '../model/roberta-large',
+                num_labels=2)
+            # model = RobertaForSequenceClassification.from_pretrained('../model/roberta-large',
+            #                                                  num_labels=2)
             self.models[index] = model
 
     def addnoise(self, x):
@@ -60,17 +67,17 @@ class Teacher:
 
         return count
 
-    def train(self, train_index,train_loaders,test_loaders,test_train,test_val):
+    def train(self, train_index, train_loaders, test_loaders, test_train, test_val):
         """Function to train all teacher models.
            Args:
                 dataset[torch tensor]: Dataset used to train teachers in format (image,label)
         """
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        
-        train_loader=train_loaders[train_index]
-        test_loader=test_loaders[train_index]
-        
-        model=self.models[0]
+
+        train_loader = train_loaders[train_index]
+        # test_loader = test_loaders[train_index]
+
+        model = self.models[0]
         model.to(device)
 
         optimizer = AdamW(model.parameters(), lr=2e-5)
@@ -91,12 +98,11 @@ class Teacher:
                 optimizer.step()
 
         model.eval()
-            
 
         predictions = []
-        true_labels = [] 
-        output=[]
-        
+        true_labels = []
+        output = []
+
         with torch.no_grad():
             for batch in test_train:
                 input_ids = batch['input_ids'].to(device)
@@ -106,6 +112,9 @@ class Teacher:
                 outputs = model(input_ids, attention_mask=attention_mask)
                 logits = outputs.logits
                 output.append(logits.cpu())
+                # print(f'outputs:{outputs}\n logits:{logits}')
+                probabilities = softmax(logits, dim=1).cpu().numpy()
+                # print(f'probabilities:{probabilities}')
 
                 predicted_labels = torch.argmax(logits, dim=1)
                 predictions.extend(predicted_labels.cpu().numpy())
@@ -114,16 +123,16 @@ class Teacher:
         accuracy = accuracy_score(true_labels, predictions)
         report = classification_report(true_labels, predictions)
 
-        print("对于test_train:")
+        print("对于student_train:")
         print(f'Teacher {train_index}, Accuracy: {accuracy:.4f}')
         print(report)
 
         predictions2 = []
-        true_labels2 = [] 
-        output2=[]
-        
+        true_labels2 = []
+        output2 = []
+
         with torch.no_grad():
-            for batch in test_val:
+            for batch in test_loaders:
                 input_ids = batch['input_ids'].to(device)
                 attention_mask = batch['attention_mask'].to(device)
                 labels = batch['label'].to(device)
@@ -131,31 +140,35 @@ class Teacher:
                 outputs = model(input_ids, attention_mask=attention_mask)
                 logits = outputs.logits
                 output2.append(logits.cpu())
+                print(logits.cpu())
 
                 predicted_labels = torch.argmax(logits, dim=1)
+                print(predicted_labels)
                 predictions2.extend(predicted_labels.cpu().numpy())
                 true_labels2.extend(labels.cpu().numpy())
+
+        # print("________________")
+        # print(f"true_labels2:{true_labels2}")
+        # print(f"predictions2:{predictions2}")
 
         accuracy = accuracy_score(true_labels2, predictions2)
         report = classification_report(true_labels2, predictions2)
 
-        print("对于test_val:")
+        print("对于teacher_test:")
         print(f'Teacher {train_index}, Accuracy: {accuracy:.4f}')
         print(report)
-
 
         model.to('cpu')
         gc.collect()
         torch.cuda.empty_cache()
-        return output,output2
+        return output, output2
 
     def load_models(self):
-    
-        path_name = "model_"
-    
-        for i in range(self.n_teachers):
-            self.models[i] = self.models[i].from_pretrained("./model/teacher_"  + str(i))
 
+        path_name = "model_"
+
+        for i in range(self.n_teachers):
+            self.models[i] = self.models[i].from_pretrained("./model/teacher_" + str(i))
 
     def aggregate(self, model_votes, batch_size):
         """Aggregate model output into a single tensor of votes of all models.
@@ -163,7 +176,7 @@ class Teacher:
                 votes: Model output
                 n_dataset: Number of datapoints
            Returns:
-                counts: Torch tensor with counts across all models    
+                counts: Torch tensor with counts across all models
            """
 
         counts = torch.zeros([batch_size, 2])
@@ -172,8 +185,8 @@ class Teacher:
 
         for model in model_votes:
             index = 0
-            for val in  model_votes[model]:
-                i=0 if val[0]>val[1] else 1
+            for val in model_votes[model]:
+                i = 0 if val[0] > val[1] else 1
                 counts[index][i] += 1
                 model_counts[model_index][index] = i
                 index += 1
@@ -182,7 +195,40 @@ class Teacher:
 
         return counts, model_counts
 
-    def save_models(self,model,model_name):
+    def aggregate_probability(self, model_votes, batch_size):
+        """Aggregate model output into a single tensor of votes of all models.
+           Args:
+                votes: Model output
+                n_dataset: Number of datapoints
+           Returns:
+                counts: Torch tensor with counts across all models
+           """
+
+        counts = torch.zeros([batch_size, 2])
+
+        for model in model_votes:
+            index = 0
+            for val in model_votes[model]:
+                # print(f'val[0]:{val[0]}; val[1]:{val[1]}')
+                i = 0 if val[0] > val[1] else 1
+                counts[index][0] += val[0]
+                counts[index][1] += val[1]
+                index += 1
+
+        # 将counts转换为概率分布
+        # probabilities = torch.nn.functional.softmax(counts, dim=1)
+
+        index = 0
+        for val in model_votes[0]:
+            counts[index][0] = counts[index][0]
+            counts[index][1] = counts[index][1]
+
+            # print(f'count[{index}]:[{counts[index][0]},counts[index][1]]:[{counts[index][1]}')
+            index += 1
+
+        return counts
+
+    def save_models(self, model, model_name):
         save_path = f'./model/{model_name}'  # 每个模型的保存路径
 
         # 检查目录是否存在，如果不存在则创建
@@ -204,7 +250,7 @@ class Teacher:
         )
         return datadepeps, dataindeps
 
-    def predict(self, data,mask,outputs):
+    def predict(self, data, mask, outputs):
         """Make predictions using Noisy-max using Laplace mechanism.
            Args:
                 data: Data for which predictions are to be made
@@ -213,26 +259,35 @@ class Teacher:
         """
 
         model_predictions = {}
+        model_probability={}
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        data=data.to(device)
-        mask=mask.to(device)
+        data = data.to(device)
+        mask = mask.to(device)
         with torch.no_grad():
             for i in range(self.n_teachers):
                 model_predictions[i] = outputs[i]
+                model_probability[i]=softmax(outputs[i], dim=1).cpu().numpy()
 
         counts, model_counts = self.aggregate(model_predictions, len(data))
         predictions_nonoise = []
+        
         for batch in counts:
-
             predictions_nonoise.append(torch.tensor(batch.max(dim=0)[1].long()).clone().detach())
 
+        # 加噪之后的概率分布
+        probability = self.aggregate_probability(model_probability, len(data))
+        # print(f'1：probability：{probability}')
+        probability = probability.apply_(self.addnoise)
+        # print(f'2:probability:{probability}')
+        probability= softmax(probability, dim=1).cpu().numpy()
+        # print(f'probability_distribution:{probability}')
 
         counts = counts.apply_(self.addnoise)
         predictions = []
         for batch in counts:
-
             predictions.append(torch.tensor(batch.max(dim=0)[1].long()).clone().detach())
 
-        output = {"predictions": predictions,"predictions_nonoise":predictions_nonoise, "counts": counts, "model_counts": model_counts}
+        output = {"predictions": predictions, "predictions_nonoise": predictions_nonoise, "counts": counts,
+                  "model_counts": model_counts, "probability_distribution": probability}
 
         return output
